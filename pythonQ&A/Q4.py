d2384388cfbe63b53588410c43fda5f637296c12
muskan-ds-coder <muskan4545783@gmail.com>
2026-09-01 00:51:26 +0530

    Q&A

---
+++ pythonQ&A/Q4.py
@@ -0,0 +1,21 @@
+# swap two numbers using third variable and without using third variable
+
+x = 13
+y = 12
+
+temp = x
+print("the value of temp variable is: ", temp)
+
+x = y 
+print("the value of x is: ", x)
+
+y = temp
+print("the value of y is: ", y)
+
+
+m = 10
+n = 20
+
+m, n = n, m
+print("the value of m is: ", m)
+print("the value of n is: ", n)
---
+++ pythonQ&A/Q5.py
@@ -0,0 +1,5 @@
+# convert kilometers to miles
+
+km = float(input("Enter the distance in kilometers: "))
+miles = km * 0.621371
+print("The distance in miles is: ", miles,"miles")
